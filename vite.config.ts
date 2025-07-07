4f0ce511bf677351c795d671ac708379c41e2c91
julinhoocorrea <83489571+julinhoocorrea@users.noreply.github.com>
2025-07-07 01:34:26 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/',  // para domínio customizado
   plugins: [react()],
   resolve: {
     alias: {
